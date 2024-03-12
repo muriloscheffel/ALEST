434956909efb1443f672ad61186e0c739c2ea52f
Murilo Scheffel <111885420+muriloscheffel@users.noreply.github.com>
2024-03-12 14:09:19 +0000

    recursao

--- 01_Recursao/src/App.java
+++ 01_Recursao/src/App.java
@@ -1,7 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("2^3 = " + potencia( + 2, 3));
+        // System.out.println("2^3 = " + potencia( + 2, 3));
+        // System.out.println("2^3 = " + potenciaRec( + 2, 3));
+        int vet1[] = {1, 2, 3, 4, 5, 6};
+        int vet2[] = {1, 2, 3};
+
+        // inverteArray(vet1);
+
+        somaVetorRec(vet2);
     }
+
 
     public static int potencia(int base, int expoente) {
         if (expoente == 0) {
@@ -13,4 +21,27 @@ public class App {
         }
         return resultado;
     }
+
+    public static int potenciaRec(int base, int expoente) {
+        if (expoente == 0) {
+            return 1;
+        }
+        return base * potenciaRec(base, expoente - 1);
+    }
+
+    public static void inverteArray(int vet[]) {
+        inverteArray(vet, 0, vet.length - 1);
+    }
+
+    public static void inverteArray(int vet[], int i, int f) {
+        if(i < f) {
+            int aux = vet[i];
+            vet[i] = vet[f];
+            inverteArray(vet, i + 1, f - 1);
+        }
+    }
+
+    //
+    // RESOLVER ALGORITMOS
+    //
 }

328891ea239103755ad97eeb59fa2560248af091
Murilo Scheffel <111885420+muriloscheffel@users.noreply.github.com>
2024-03-15 01:33:11 +0000

    add, clear

--- 01_ArrayList/src/App.java
+++ 01_ArrayList/src/App.java
@@ -1,11 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        ListArrayOfInteger lista = new ListArrayOfInteger(10);
+        ListArrayOfInteger lista = new ListArrayOfInteger();
 
-        lista.add(2);
+        lista.add(5);
+        lista.add(8);
+        lista.add(10);
+
+        lista.add(0, 10);
 
         System.out.println(lista);
 
         System.out.println(lista.size());
+
+        lista.clear();
+
+        System.out.println("--------");
+
+        System.out.println(lista);
     }
 }
--- 01_ArrayList/src/ListArrayOfInteger.java
+++ 01_ArrayList/src/ListArrayOfInteger.java
@@ -5,7 +5,7 @@ public class ListArrayOfInteger{
     private int count;
 
     public ListArrayOfInteger(){
-        data = new int[10];
+        data = new int[1];
         count = 0;
     }
 
@@ -14,16 +14,44 @@ public class ListArrayOfInteger{
         count = 0;
     }
 
+    public void add(int index, int i) {
+        if(index > count) {
+            throw new IllegalArgumentException("Index inválido");
+        }
+
+        int[] aux = new int[data.length + 1];
+
+        for(int j = 0; j < aux.length; j++) {
+            if (j == index) {
+               aux[index] = i; 
+            } else {
+                if(j > index) {
+                    aux[j] = data[j - 1];
+                }
+                else {
+                    aux[j] = data[j];
+                }
+            }
+        }
+        data = aux;
+        count++;
+    }
+
     public void add(int i) {
-        if(count < data.length) {
-            expandirArray();
+        if(count == data.length) {
+            expandirArray(data.length + 1);
         }
         data[count] = i;
         count++;
     }
 
-    public void expandirArray() {
+    public void expandirArray(int tam) {
+        int[] aux = new int[tam];
 
+        for(int j = 0; j < data.length; j++) {
+            aux[j] = data[j];
+        }
+        data = aux;
     }
 
     public void remove(int index) {
@@ -39,16 +67,17 @@ public class ListArrayOfInteger{
     }
 
     public void clear() {
-
+        int[] aux = new int[0];
+        data = aux;
     }
 
-    public boolean contains() {
+    // public boolean contains() {
 
-    }
+    // }
 
-    public boolean isEmpty() {
-        
-    }
+    // public boolean isEmpty() {
+
+    // }
 
     @Override
     public String toString() {

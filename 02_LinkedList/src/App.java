e29a12fbb6df6b9b9f019f691d2d17a77691dc5f
Murilo Scheffel <111885420+muriloscheffel@users.noreply.github.com>
2024-04-02 14:07:08 +0000

    first commit

---
+++ 02_LinkedList/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        LinkedListOfInteger l = new LinkedListOfInteger();
+        l.add(2);
+        l.add(25);
+        l.add(3);
+        l.add(10);
+
+        
+        System.out.println(l);
+    }
+}

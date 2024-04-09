58d1290c32a6cd8a4577dea0ab999722e322b768
Murilo Scheffel <111885420+muriloscheffel@users.noreply.github.com>
2024-04-09 23:59:05 +0000

    first commit

--- T1_ALEST_I/src/Algoritmos.java
+++ T1_ALEST_I/src/Algoritmos.java
@@ -13,4 +13,19 @@ public class Algoritmos {
         }
         return cont_op;
     }
+
+    // Algoritmo 2
+    public int f( int n ) {
+        int i, j, k, res = 0;
+        int cont_op = 0;
+        for( i = n; i <= n; i += i/2+1 ) {
+            for( j = i/2; j <= i*i; j += i+1 ) {
+                for( k = n; k <= 2*n; k += i+1 ) {       
+                    res = res + n;
+                    cont_op++;
+                }
+            }
+        }
+        return cont_op;
+    }
 }

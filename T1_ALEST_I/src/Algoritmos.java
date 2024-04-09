68af95b9a214b7ab8ede5d8d36d3d5a6e07a9b80
Murilo Scheffel <111885420+muriloscheffel@users.noreply.github.com>
2024-04-09 23:56:11 +0000

    algoritmos 1, 2

--- T1_ALEST_I/src/Algoritmos.java
+++ T1_ALEST_I/src/Algoritmos.java
@@ -1,3 +1,16 @@
 public class Algoritmos {
-    
+    // Algoritmo 1
+    public int f1(int n) {
+        int i, j, k, res = 0;
+        int cont_op = 0;
+        for (i = 1; i <= n * n; i += 1) {
+            for (j = 1; j <= i; j += 2) {
+                for (k = n + 1; k <= 2 * i; k += i * j) {
+                    res = res + k + 1;
+                    cont_op++;
+                }
+            }
+        }
+        return cont_op;
+    }
 }
--- T1_ALEST_I/src/App.java
+++ T1_ALEST_I/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Algoritmos f  = new Algoritmos();
+        
+        for(int n=10; n<1000; n+=50) {
+            int r = f.f1(n); // Chame uma a uma as funcoes para 
+                             // analisar os seus comportamentos.
+            System.out.println(n+";"+r);
+        }
     }
 }

16807c58204ddfbdaae6e0bfe32de909073a6438
Murilo Scheffel <111885420+muriloscheffel@users.noreply.github.com>
2024-04-09 23:59:27 +0000

    c

--- T1_ALEST_I/src/Algoritmos.java
+++ T1_ALEST_I/src/Algoritmos.java
@@ -1,8 +1,10 @@
 public class Algoritmos {
+    
     // Algoritmo 1
     public int f1(int n) {
         int i, j, k, res = 0;
         int cont_op = 0;
+
         for (i = 1; i <= n * n; i += 1) {
             for (j = 1; j <= i; j += 2) {
                 for (k = n + 1; k <= 2 * i; k += i * j) {
@@ -18,6 +20,7 @@ public class Algoritmos {
     public int f( int n ) {
         int i, j, k, res = 0;
         int cont_op = 0;
+
         for( i = n; i <= n; i += i/2+1 ) {
             for( j = i/2; j <= i*i; j += i+1 ) {
                 for( k = n; k <= 2*n; k += i+1 ) {       

ae22ea62332b80a3beea3cebf15b363813259244
Murilo Scheffel <111885420+muriloscheffel@users.noreply.github.com>
2024-04-10 23:15:30 +0000

    algoritmos

--- T1_ALEST_I/src/Algoritmos.java
+++ T1_ALEST_I/src/Algoritmos.java
@@ -1,5 +1,5 @@
 public class Algoritmos {
-    
+
     // Algoritmo 1
     public int f1(int n) {
         int i, j, k, res = 0;
@@ -17,14 +17,60 @@ public class Algoritmos {
     }
 
     // Algoritmo 2
-    public int f( int n ) {
+    public int f2(int n) {
         int i, j, k, res = 0;
         int cont_op = 0;
 
-        for( i = n; i <= n; i += i/2+1 ) {
-            for( j = i/2; j <= i*i; j += i+1 ) {
-                for( k = n; k <= 2*n; k += i+1 ) {       
+        for (i = n; i <= n; i += i / 2 + 1) {
+            for (j = i / 2; j <= i * i; j += i + 1) {
+                for (k = n; k <= 2 * n; k += i + 1) {
                     res = res + n;
+                    cont_op++;
+                }
+            }
+        }
+        return cont_op;
+    }
+
+    // Algoritmo 3
+    public int f3(int n) {
+        int i, j, k, res = 0;
+        int cont_op = 0;
+
+        for (i = 1; i <= n * n; i += 2) {
+            for (j = i / 2; j <= 2 * i; j += i / 2 + 1) {
+                for (k = j + 1; k <= n + j; k += k / 2 + 1) {
+                    res = res + Math.abs(j - i);
+                    cont_op++;
+                }
+            }
+        }
+        return cont_op;
+    }
+
+    // Algoritmo 4
+    public int f4(int n) {
+        int i, j, k, res = 0;
+        int cont_op = 0;
+        for (i = 1; i <= n * n; i += 2) {
+            for (j = 2; j <= n + 1; j += i + 1) {
+                for (k = j / 2; k <= i + 1; k += j / 2 + 1) {
+                    res = res + j + 1;
+                    cont_op++;
+                }
+            }
+        }
+        return cont_op;
+    }
+
+    // Algoritmo 5
+    public int f5(int n) {
+        int i, j, k, res = 0;
+        int cont_op = 0;
+        for (i = n / 2; i <= n; i += 3) {
+            for (j = i; j <= i * i; j += 2) {
+                for (k = i; k <= 2 * j; k += 1) {
+                    res = res + n + j;
                     cont_op++;
                 }
             }
--- T1_ALEST_I/src/App.java
+++ T1_ALEST_I/src/App.java
@@ -2,10 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         Algoritmos f  = new Algoritmos();
         
-        for(int n=10; n<1000; n+=50) {
-            int r = f.f1(n); // Chame uma a uma as funcoes para 
-                             // analisar os seus comportamentos.
-            System.out.println(n+";"+r);
+        for(int n = 10; n < 1000; n += 50) {
+            int r = f.f1(n); // Chame uma a uma as funcoes para analisar os seus comportamentos.
+            System.out.println(n + ";" + r);
         }
     }
 }
